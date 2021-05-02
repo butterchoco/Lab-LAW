247b56164381460550cc03992b3bf8eb7a242665
butterchoco <ahmadsupri1999@gmail.com>
2021-05-02 07:33:53 +0700

    Finish chat application

---
+++ Lab3-1706075016/backend/index.js
@@ -0,0 +1,42 @@
+var amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    channel.assertExchange("time", "fanout", { durable: false });
+
+    let time;
+    setInterval(() => {
+      time = new Date().toLocaleTimeString();
+      channel.publish("time", "", Buffer.from(time));
+    }, 1000);
+
+    var queue = "room-public";
+
+    channel.assertQueue(queue, {
+      durable: true,
+    });
+    channel.prefetch(1);
+    console.log(" [x] Awaiting RPC requests");
+    channel.consume(queue, function reply(msg) {
+      const response = JSON.parse(msg.content.toString());
+      response["createdAt"] = new Date()
+        .toLocaleTimeString()
+        .split(":")
+        .slice(0, 2)
+        .join(":");
+      channel.publish(
+        queue,
+        msg.properties.headers["room-id"],
+        Buffer.from(JSON.stringify(response))
+      );
+      channel.ack(msg);
+    });
+  });
+});
---
+++ Lab3-1706075016/frontend/index.js
@@ -0,0 +1,66 @@
+if (!("WebSocket" in window)) alert("Websocket is not supported in browser");
+
+var user;
+if (!window.localStorage.getItem("user")) user = prompt("Masukkan username:");
+window.localStorage.setItem("user", user);
+const urlParams = new URLSearchParams(window.location.search);
+var roomId = urlParams.get("id") || "test";
+
+var ws = new WebSocket("ws://localhost:15674/ws");
+var client = Stomp.over(ws);
+client.debug = null;
+
+const onSendMessage = (e) => {
+  console.log("test");
+  const message = $(".message__input input[type=text]").val();
+  client.send(
+    "/queue/room-public",
+    {
+      "room-id": roomId,
+    },
+    JSON.stringify({
+      user: window.localStorage.getItem("user"),
+      message: message,
+    })
+  );
+  $(".message__input input[type=text]").val("");
+};
+
+$("#send-message").on("click", onSendMessage);
+$(".message__input input[type=text]").on("keyup", (e) => {
+  if (e.key === "enter" || e.keyCode === 13) onSendMessage(e);
+});
+
+var onConnect = function () {
+  console.log("connected");
+  client.subscribe("/exchange/room-public/" + roomId, onMessage);
+  client.subscribe("/exchange/time/stream", onTimeMessage);
+};
+
+var onError = function (e) {
+  console.log(e);
+};
+
+var onTimeMessage = function (m) {
+  $("#time").html(m.body);
+};
+
+var onMessage = function (m) {
+  const response = JSON.parse(m.body);
+  let perspective = "other";
+  if (window.localStorage.getItem("user") === response.user) perspective = "me";
+  $(".message__container").append(
+    templateChat(
+      perspective,
+      response.user,
+      response.message,
+      response.createdAt
+    )
+  );
+};
+
+const templateChat = (perspective, user, message, createdAt) => {
+  return `<div class="message__chat message__chat--${perspective}"><p class="message__chat__user">${user}</p><p class="message__chat__content">${message}</p><p class="message__chat__time">${createdAt}</p></div>`;
+};
+
+client.connect("guest", "guest", onConnect, onError, "/");
