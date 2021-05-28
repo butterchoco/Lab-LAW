75f82228cce40cad4e1a7d0c1b72893f0fc59fba
butterchoco <ahmadsupri1999@gmail.com>
2021-05-29 04:04:40 +0700

    Finish rabbitmq server

--- Mini3-1706075016/server/index.js
+++ Mini3-1706075016/server/index.js
@@ -1,3 +1,4 @@
+const amqp = require("amqplib/callback_api");
 const { spawn } = require("child_process");
 const { v4: uuidv4 } = require("uuid");
 const {
@@ -17,20 +18,42 @@ Server.addService(MiniProto.MiniProjectService.service, {
         details: "Not found",
       });
     }
-    const download = spawn("wget", [url, "-O", `${uuidv4}.`]);
-    download.stderr.on("data", (data) => {
-      const str = data.toString();
-      if (str.includes("%")) {
-        const progress = str.match(/ (.*)%/)[0];
-        console.log(progress);
-      } else if (str.includes("saved")) {
-        const filename = str.match(/‘(.*)’/)[1];
-        console.log(filename);
+
+    amqp.connect("amqp://localhost", (error0, { createChannel }) => {
+      if (error0) {
+        throw error0;
       }
-    });
-    download.on("close", () => {
-      let response = { url, id };
-      callback(null, response);
+      createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+
+        const npm = "1706075016";
+        const id = uuidv4();
+        channel.assertExchange(npm, "topic", { durable: false });
+        const publish = {
+          json: function (obj) {
+            channel.publish(npm, id, Buffer.from(JSON.stringify(...obj)));
+          },
+        };
+
+        const download = spawn("wget", [url, "-P", "../saved"]);
+        let filename;
+        download.stderr.on("data", (data) => {
+          const str = data.toString();
+          if (str.includes("%")) {
+            const progress = str.match(/(.*) (.*)%/)[2];
+            publish.json(progress);
+          } else if (str.includes("saved")) {
+            filename = str.match(/‘(.*)’/)[1];
+          }
+        });
+        download.on("close", () => {
+          publish.json({ url: "http://localhost:3000/download/" + filename });
+        });
+        let response = { url, id };
+        callback(null, response);
+      });
     });
   },
 });
